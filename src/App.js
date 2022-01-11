a36d2ddb4c22ae122918ab8ce896c3e8d9cc96c9
BekembaevaSel <bekembaeva8@gmail.com>
2022-01-11 13:08:32 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import Expenses from './components/Expenses'
+
+function App() {
+  const expenses = [
+		{
+			id: 'el1',
+			title: 'paper',
+			amount: 1.5,
+			date: new Date(2021, 4, 3),
+		},
+		{
+			id: 'el2',
+			title: 'car insurance',
+			amount: 1.5,
+			date: new Date(2021, 3, 22),
+		},
+		{
+			id: 'el3',
+			title: 'painting',
+			amount: 10,
+			date: new Date(2021, 4, 12),
+		},
+		{
+			id: 'el4',
+			title: 'refil',
+			amount: 5,
+			date: new Date(2021, 6, 12),
+		},
+	]
+  return (
+    <div className="App">
+      <Expenses data={expenses}/>
+    </div>
+  );
+}
+
+export default App;
