8c9a6002e8267fcdb83c68eb44bb6c4065695620
BekembaevaSel <bekembaeva8@gmail.com>
2022-01-17 19:45:02 +0600

    added button updated and useState

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,21 +3,24 @@ import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
-	const [title, setTitle] = useState(props.title)
-	const [amount, setAmount] = useState('$200')
+	let [title, setTitle] = useState(props.title)
+	// const [amount, setAmount] = useState('$200')
 	const expenseTitle = props.title
-	const expenseAmount = props.amount
+	// const expenseAmount = props.amount
 
 	const updateTitleHandler = () => {
 		setTitle('updated')
-		expenseTitle = 'updated'
+		// expenseTitle = 'updated'
 	}
+
+	
 
 	return (
 		<div className='expense-item'>
+			{console.log(title)}
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
 			</div>
 			<button onClick={updateTitleHandler}>update</button>
